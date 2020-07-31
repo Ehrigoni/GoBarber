34c59ae4f2d80d83bd85f15fa4eec2009d74cf83
Ehrigoni <ehrigoni@gmail.com>
2020-07-31 15:35:47 -0300

    beggining

--- src/database/migrations/1596209424914-CreateAppointments.ts
+++ src/database/migrations/1596209424914-CreateAppointments.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1596209424914 implements MigrationInterface {
+export default class CreateAppointments1596209424914 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
@@ -11,16 +11,25 @@ export class CreateAppointments1596209424914 implements MigrationInterface {
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
                         type: 'varchar',
-                        isNullable: false,
                     },
                     {
                         name: 'date',
                         type: 'timestamp with time zone',
-                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
                     }
                 ]
             })
